2bf4ddfe2a8b2a17bfa681ccbe7250e05a4d6379
Zurkon <lukeyoshioka@gmail.com>
2021-01-22 20:13:16 -0300

    Added background video on HeroSection component

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+import backgroundVideo from '../../assets/video.mp4';
+
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './styles';
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  }
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={backgroundVideo} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Backing Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
